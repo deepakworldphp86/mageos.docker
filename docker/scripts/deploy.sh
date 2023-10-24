fd4bf1c2ebfa6a7a115bd0404812090b287bbb03
Deepak <deepakworldphp86@gmail.com>
2023-10-24 17:12:33 +0530

    Change Deploy

--- docker/scripts/deploy.sh
+++ docker/scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 clear
 
-if [ ! $1 ]
+if [ "$1" == "full" ] 
 then
 	echo "*********************** Upgrading setup - setup:upgrade ****************************" 
-	 php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento setup:upgrade
+	php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento setup:upgrade
 	echo "*********************** Setup compile - setup:di:compile *******************************" 
 	php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento setup:di:compile
 	echo "*********************** setup reindex - indexer:reindex ********************************"
@@ -15,14 +15,44 @@ then
 fi
 
 
-
-if [ $1 == "compile" ]
+if [ "$1" == "compile" ] 
 then
 
 	echo "*********************** Setup compile - setup:di:compile *******************************" 
 	php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento setup:di:compile
 
 fi
+if [ "$1" == "index" ] 
+then
+
+	echo "*********************** setup reindex - indexer:reindex ********************************"
+	php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento indexer:reindex
+
+fi
+if [ "$1" == "content" ] 
+then
+
+	echo "*********************** setup static content deploy - setup:static-content:deploy -f  *******************" 
+	php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento setup:static-content:deploy -f
+
+fi
+if [ "$1" == "upgrade_content" ] 
+then
+
+	echo "*********************** Upgrading setup - setup:upgrade ****************************" 
+	php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento setup:upgrade
+	echo "*********************** setup static content deploy - setup:static-content:deploy -f  *******************" 
+	php -d memory_limit=-1 /var/www/html/${MAGE_INSTALLATION_FOLDER}/bin/magento setup:static-content:deploy -f
+
+fi
+if [ "$1" == "permission" ] 
+then
+
+	echo "*********************** Giving Permission *******************" 
+	chmod -R 777 /var/www/html/${MAGE_INSTALLATION_FOLDER}/
+	echo "*********************** Ending Permission *******************" 
+
+fi
 
 
- #chmod -R 777 /var/www/html/${MAGE_INSTALLATION_FOLDER}/
+ exit 1
